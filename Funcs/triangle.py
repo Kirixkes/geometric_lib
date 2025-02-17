047a65f198a37e19bb7d1706f5cddbf29e26f0b1
Alpatov Kirill <464984@niuitmo.ru>
2025-02-18 00:06:35 +0300

    Update triangle.py

--- Funcs/triangle.py
+++ Funcs/triangle.py
@@ -1,15 +1,16 @@
 from math import sqrt
 
-
 def area(a, b, c):
-    if isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)):
-        return sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c))
+    if (isinstance(a, (int, float)) and isinstance(b, (int, float)) 
+            and isinstance(c, (int, float))):
+        s = (a + b + c) / 2
+        return sqrt(s * (s - a) * (s - b) * (s - c))
     else:
         print("Wrong input")
 
-
 def perimeter(a, b, c):
-    if isinstance(a, (int, float)) and isinstance(b, (int, float)) and isinstance(c, (int, float)):
+    if (isinstance(a, (int, float)) and isinstance(b, (int, float)) 
+            and isinstance(c, (int, float))):
         return a + b + c
     else:
         print("Wrong input")
